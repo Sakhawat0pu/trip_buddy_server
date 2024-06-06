ee41fc3ed3214d49c78e9d80f54a62a1cfc1f575
Sakhawat0pu <s19hossain98@gmail.com>
2024-06-06 01:32:25 -0400

    changed cors origin

--- src/app.ts
+++ src/app.ts
@@ -9,15 +9,14 @@ import config from "./app/config";
 // Create an instance of Express application
 const app: Application = express();
 //http://localhost:3000
+// https://ct-travel-buddy.netlify.app
 // Middleware to parse incoming JSON and text requests
 app.use(express.json());
 app.use(express.text());
 // Middleware to parse incoming URL-encoded requests
 app.use(express.urlencoded({ extended: true }));
 // Middleware to enable Cross-Origin Resource Sharing (CORS) with credentials support
-app.use(
-	cors({ origin: "https://ct-travel-buddy.netlify.app", credentials: true })
-);
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 // Middleware to parse cookies from incoming requests
 app.use(cookieParser());
 
